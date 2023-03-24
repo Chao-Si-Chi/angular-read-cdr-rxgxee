6ab3e2ac6377db59ab7a0cbc6bc4b2caa7a740c2
Chao-Si-Chi <128582972+Chao-Si-Chi@users.noreply.github.com>
2023-03-24 17:29:59 +0800

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,79 @@ import { Service } from './service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular';
+  /*Products: Product[] = [
+    {
+        'id': 11,
+        'title': 'OPPO F5 Youth (Gold, 32 GB)',
+        'modelName': 'F5 Youth',
+        'color': 'Gold',
+        'productType': 'Mobile',
+        'brand': 'OPPO',
+        'price': 16990
+    },
+    {
+        'id': 12,
+        'title': 'Dell Inspiron 7000',
+        'modelName': 'Inspiron',
+        'color': 'Gray',
+        'productType': 'Laptop',
+        'brand': 'DELL',
+        'price': 59990
+    }
+  ];*/
+
+  name = "Angular";
+  //allowNewProduct = false;
+  private _allowNewProduct = false;
+  get allowNewProduct(){
+    return this._allowNewProduct;
+  }
+  set allowNewProduct(value){
+    this._allowNewProduct = value;
+  }
+  //New Product Item
+  newProduct = { title: "", productType: "", price: 0 };
+  products: Product[];
+
+  constructor() { 
+    this.products = [
+      {
+        id: 11,
+        title: "OPPO F5 Youth (Gold, 32 GB)",
+        modelName: "F5 Youth",
+        color: "Gold",
+        productType: "Mobile",
+        brand: "OPPO",
+        price: 16990
+      },
+      {
+        id: 12,
+        title: "Dell Inspiron 7000",
+        modelName: "Inspiron",
+        color: "Gray",
+        productType: "Laptop",
+        brand: "DELL",
+        price: 59990
+      }
+    ];
+
+    setTimeout(()=>{
+      this.allowNewProduct = true;
+    }, 2000)
+  }
+
+  onNewProductClick(){
+    //alert("new botton click!!");
+    this.showNewProductPanel = !this.showNewProductPanel;
+  }
+
+  getColor(product) {
+    console.log(product);
+    return product.productType === 'Mobile' ? 'red' : 'black'
+  }
+
+  onDelete(id){
+    console.log('in app component: ',id);
+    this.products.splice(this.products.findIndex(x=>x.id === id), 1);
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,64 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../models/product.ts';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+//import { Product } from '../models/product.ts';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  @Input('productData') products: any;
+  @Output() deletedItem: EventEmitter<any> = new EventEmitter();
+  constructor(){
 
-  Products: Product[] = [
-    {
-        'id': 11,
-        'title': 'OPPO F5 Youth (Gold, 32 GB)',
-        'modelName': 'F5 Youth',
-        'color': 'Gold',
-        'productType': 'Mobile',
-        'brand': 'OPPO',
-        'price': 16990
-    },
-    {
-        'id': 12,
-        'title': 'Dell Inspiron 7000',
-        'modelName': 'Inspiron',
-        'color': 'Gray',
-        'productType': 'Laptop',
-        'brand': 'DELL',
-        'price': 59990
-    }
-  ];
-
-  name = "Angular";
-  //allowNewProduct = false;
-  private _allowNewProduct = false;
-  get allowNewProduct(){
-    return this._allowNewProduct;
   }
-  set allowNewProduct(value){
-    this._allowNewProduct = value;
-  }
-  //New Product Item
-  newProduct = { title: "", productType: "", price: 0 };
-  product: Product[];
-
-  constructor() { 
-
-    setTimeout(()=>{
-      this.allowNewProduct = true;
-    }, 2000)
-  }
-
-  onNewProductClick(){
-    //alert("new botton click!!");
-    this.showNewProductPanel = !this.showNewProductPanel;
-  }
-
-  getColor(product) {
-    console.log(product);
-    return product.productType === 'Mobile' ? 'red' : 'black'
-  }
-
+  
   ngOnInit() {
+  }
+
+  onDelete(product) {
+    console.log(product);
+    this.deletedItem.emit(product.id);
   }
 
 }
